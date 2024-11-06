a4091c13a2bbf875dfe77117851aa09d34741db2
Eugene <cigan-yashka@yandex.ru>
2024-11-07 01:52:05 +0300

    feat:new function

--- src/js/script.js
+++ src/js/script.js
@@ -1,44 +1,15 @@
-// Находим все кнопки с классом 'city__button'
-const buttons = document.querySelectorAll('.city__button');
+function showGift(index) {
+    const buttons = document.querySelectorAll('.gift__button');
+    const images = document.querySelectorAll('.gift__img');
 
-// Находим элемент изображения, который нужно менять
-const imageElement = document.querySelector('.remote__img');
+    // Скрываем кнопку и показываем соответствующую картинку
+    buttons[index].style.display = 'none';
+    images[index].style.display = 'block';
 
-// Проходим по каждой кнопке
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        // Убираем активный класс у всех кнопок
-        buttons.forEach(btn => btn.classList.remove('city__button-active'));
-
-        // Добавляем активный класс текущей кнопке
-        button.classList.add('city__button-active');
-
-        // Меняем изображение в зависимости от кнопки
-        if (button.id === 'spbButton') {
-            imageElement.src = 'img/spb.png';
-        } else if (button.id === 'moscowButton') {
-            imageElement.src = 'img/msk.png';
-        }
+    // Запуск анимации фейерверка
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 }
     });
-});
-
-// Массив с путями к изображениям
-const images = ['img/studio1.png', 'img/studio2.png', 'img/studio3.png'];
-
-// Индекс текущего изображения
-let currentIndex = 0;
-
-// Функция для смены изображений
-function changeImage() {
-    // Получаем элемент по id
-    const imgElement = document.getElementById('studioImage');
-    
-    // Меняем src изображения на следующее
-    imgElement.src = images[currentIndex];
-    
-    // Увеличиваем индекс, сбрасывая его до 0, если достигнут конец массива
-    currentIndex = (currentIndex + 1) % images.length;
-}
-
-// Запускаем функцию смены изображений каждые 5 секунд (5000 мс)
-setInterval(changeImage, 5000);
+};
